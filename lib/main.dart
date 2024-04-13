d7da93321f41436b88737f14b43ca0f06afc50ce
Huvee <wongrenthou1265@gmail.com>
2024-04-13 16:57:25 +0800

    feat: Routing Navigation and Signup
    \

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/welcome.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -16,34 +15,60 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:
-            AppBar(backgroundColor: Colors.blue, title: Text('Hello World')),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Hello World'),
+        ),
         body: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.end,
-          children: const [
+          children: [
             Icon(Icons.backpack),
             Icon(Icons.leaderboard),
-            Icon(Icons.access_alarm)
+            Icon(Icons.access_alarm),
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('Button Clicked');
-          },
-          child: const Icon(Icons.add),
+        floatingActionButton: Builder(
+          builder: (context) => FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Welcome()),
+              );
+            },
+            backgroundColor: Colors.green,
+            child: const Icon(Icons.lock),
+          ),
+        ),
+        drawer: Drawer(
+          child: ListTile(
+            title: Text('Drawer'),
+          ),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Settings'),
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
             BottomNavigationBarItem(
-                icon: Icon(Icons.notifications), label: 'Notifications'),
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_box),
+              label: 'Profile',
+            ),
           ],
-        ),
-        drawer: Drawer(
-          child: Text('Drawer'),
+          onTap: (index) {
+            if (index == 0) {
+              print('Home');
+            } else if (index == 1) {
+              print('Search');
+            } else if (index == 2) {
+              print('Profile');
+            }
+          },
         ),
       ),
     );
---
+++ lib/screens/welcome.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/signin.dart';
+import 'package:flutter_application_1/screens/signup.dart';
+import 'package:flutter_application_1/theme/theme.dart';
+import 'package:flutter_application_1/widgets/custom_scaffold.dart';
+import 'package:flutter_application_1/widgets/welcome_button.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(
+              flex: 8,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 0,
+                  horizontal: 40.0,
+                ),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'Welcome Back!\n',
+                            style: TextStyle(
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w600,
+                            )),
+                        TextSpan(
+                            text:
+                                '\nEnter personal details to your employee account',
+                            style: TextStyle(
+                              fontSize: 20,
+                              // height: 0,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+          Flexible(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Sign in',
+                      onTap: SignIn(),
+                      color: Colors.transparent,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Sign up',
+                      onTap: const SignUp(),
+                      color: Colors.white,
+                      textColor: lightColorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
